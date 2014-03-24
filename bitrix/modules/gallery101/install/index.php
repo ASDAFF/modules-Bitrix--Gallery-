99cb978d1bb46e15e4528af86fe8d42e04a98b16
maxouni <maxonmishin@gmail.com>
2014-03-24 16:39:01 +0400

    создал файловую структуру модуля

---
+++ bitrix/modules/gallery101/install/index.php
@@ -0,0 +1,58 @@
+<?
+
+Class gallery101 extends CModule
+{
+var $MODULE_ID = "st101.gallery101";
+var $MODULE_VERSION;
+var $MODULE_VERSION_DATE;
+var $MODULE_NAME;
+var $MODULE_DESCRIPTION;
+var $MODULE_CSS;
+
+function dv_module()
+{
+$arModuleVersion = array();
+
+$path = str_replace("\\", "/", __FILE__);
+$path = substr($path, 0, strlen($path) - strlen("/index.php"));
+include($path."/version.php");
+
+if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion))
+{
+$this->MODULE_VERSION = $arModuleVersion["VERSION"];
+$this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+}
+
+$this->MODULE_NAME = "studio 101 – модуль галереи";
+$this->MODULE_DESCRIPTION = "После установки вы сможете пользоваться компонентом st101:gallery.albums";
+}
+
+function InstallFiles($arParams = array())
+{
+CopyDirFiles($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/gallery101/install/components", $_SERVER["DOCUMENT_ROOT"]."/bitrix/components", true, true);
+return true;
+}
+
+function UnInstallFiles()
+{
+DeleteDirFilesEx("/bitrix/components/st101");
+return true;
+}
+
+function DoInstall()
+{
+global $DOCUMENT_ROOT, $APPLICATION;
+$this->InstallFiles();
+RegisterModule("dv_module");
+$APPLICATION->IncludeAdminFile("Установка модуля галереи от Студии 101", $DOCUMENT_ROOT."/bitrix/modules/dv_module/install/step.php");
+}
+
+function DoUninstall()
+{
+global $DOCUMENT_ROOT, $APPLICATION;
+$this->UnInstallFiles();
+UnRegisterModule("dv_module");
+$APPLICATION->IncludeAdminFile("Деинсталляция модуля модуля галереи от Студии 101", $DOCUMENT_ROOT."/bitrix/modules/dv_module/install/unstep.php");
+}
+}
+?>
---
+++ gallery/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "MaxiGallery");
+$APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
+$APPLICATION->SetTitle("Фотогалерея");
+?>
+<?
+
+?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
